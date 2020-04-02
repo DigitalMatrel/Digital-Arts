b68099260abf8ece75cfa4d161ce5757bcdc3957
DigitalMatrel <63007716+DigitalMatrel@users.noreply.github.com>
2020-04-02 15:18:38 +0100

    Update solwalldrawing.js

--- solwalldrawing.js
+++ solwalldrawing.js
@@ -1,64 +1,71 @@
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	strokeWeight(2);
+	strokeWeight(1);
 	rectMode(CENTER);
 }
 
+	var xPos = 5;
+	var yPos = 5;
+	var xPos2 = 5;
+	var yPos2 = 5;
+	var ww = 0;
+	var wh = 0;
 
 
 function draw() {
+		ww = windowWidth;
+		wh = windowHeight;
 	//Block 1 - Vertical Lines
-	var xPos = 5;
-	var yPos = 5;
-	while(xPos < (windowWidth * 0.19)) {
-		line(xPos,5,xPos,(windowHeight - 5));
-		xPos = xPos + 4;
+
+	for(i=xPos; i < (ww * 0.19); i += 4) {
+		line(i,5,i,(wh - 5));
 	}
 	//Block 2 - Horizontal Lines
-	while(yPos < (windowHeight-4)) {
-		line((windowWidth * 0.27),yPos,(windowWidth * 0.46),yPos);
-		yPos = yPos + 4;
+	for(let i=yPos; i < (wh-4); i += 4) {
+		line((ww * 0.27),i,(ww * 0.46),i);
 	}
+	
 	//Block 3 - Diagonal Lines Left to Right
-	var xPos2 = ((windowWidth * 0.54)+4);
-	var yPos2 = 5;
-	xPos = (windowWidth * 0.54);
+	xPos2 = ((ww * 0.54)+4);
+	yPos2 = 5;
+	xPos = (ww * 0.54);
 	yPos = 9
-	while(xPos2 < (windowWidth * 0.73)) {
+	
+	while(xPos2 < (ww * 0.73)) {
 		line(xPos, yPos, xPos2, yPos2);
-		xPos2 = xPos2 + 8;
-		yPos = yPos + 8;
+		xPos2 += 8;
+		yPos += 8;
 	}
-	while(yPos < (windowHeight - 9)) {
+	while(yPos < (wh - 9)) {
 		line(xPos, yPos, xPos2, yPos2);
-		yPos = yPos + 8;
-		yPos2 = yPos2 + 8;
+		yPos += 8;
+		yPos2 += 8;
 	}
-	while (xPos < ((windowWidth * 0.73) + 4)) {
+	while (xPos < ((ww * 0.73) + 4)) {
 		line(xPos, yPos, xPos2, yPos2);
-		yPos2 = yPos2 + 8;
-		xPos = xPos + 8;
+		yPos2 += 8;
+		xPos += 8;
 	}
+	
 	//Block 4 - Diagonal Lines Right to Left
-	xPos2 = (windowWidth - 5);
+	xPos2 = (ww - 5);
 	yPos2 = 9;
-	xPos = (windowWidth - 9);
+	xPos = (ww - 9);
 	yPos = 5
-	while(xPos > (windowWidth * 0.81)) {
+	while(xPos > (ww * 0.81)) {
 		line(xPos2, yPos2, xPos, yPos);
-		xPos = xPos - 8;
-		yPos2 = yPos2 + 8;
+		xPos -= 8;
+		yPos2 += 8;
 	}
-	while(yPos2 < (windowHeight - 9)) {
+	while(yPos2 < (wh - 9)) {
 		line(xPos2, yPos2, xPos, yPos);
-		yPos = yPos + 8;
-		yPos2 = yPos2 + 8;
+		yPos += 8;
+		yPos2 += 8;
 	}
-	while (yPos < (windowHeight - 9)) {
+	while (yPos < (wh - 8)) {
 		line(xPos2, yPos2, xPos, yPos);
-		yPos = yPos + 8;
-		xPos2 = xPos2 - 8;
+		yPos += 8;
+		xPos2 -= 8;
 	}
 }
-
