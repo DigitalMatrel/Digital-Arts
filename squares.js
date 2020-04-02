b5a7e18ac4306e264fa57ce45290d547937a89b5
DigitalMatrel <63007716+DigitalMatrel@users.noreply.github.com>
2020-04-02 15:48:15 +0100

    Create squares.js

---
+++ squares.js
@@ -0,0 +1,25 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+	rectMode(CENTER);
+	stroke(2);
+}
+
+var ww=0;
+var wh=0;
+var xw=0;
+var yh=0;
+
+function draw() {
+	ww=windowWidth;
+	wh=windowHeight;
+	xw=windowWidth/20;
+	yh=windowHeight/20;
+	
+	
+	for(let x=xw/2; x<ww; x+=xw){
+		for(let y=yh/2; y<wh; y+=yh){
+			rect(x,y,(xw*0.9),(yh*0.9));
+		}
+	}
+}
